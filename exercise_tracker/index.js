801392153fd26877abc8aebcd9b3a5385c0d0b77
Andres Flores <andresflo883@gmail.com>
2022-07-31 21:52:13 -0400

    search user's log from (date1) to (date2)

--- exercise_tracker/index.js
+++ exercise_tracker/index.js
@@ -17,15 +17,12 @@ mongoose.connect(
   "mongodb+srv://aflo7:November2000@cluster0.gppnw.mongodb.net/exercise_project?retryWrites=true&w=majority"
 )
 
-
 const User = new mongoose.Schema({
   username: { type: String, required: true },
   log: [{ desc: String, duration: Number, date: String }],
   exerciseCount: { type: Number, default: 0 }
 })
 const UserModel = mongoose.model("UserModel", User)
-
-
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
@@ -36,25 +33,22 @@ app.get("/api/users/displaydata/:username", function (req, res) {
   UserModel.findOne(
     { username: req.params.username },
     function (err, userThatWasFound) {
-      if (err) { 
+      if (err) {
         return console.error(err)
-        
       }
       if (!userThatWasFound) {
-        return res.sendFile(__dirname + '/views/404.html');
+        return res.sendFile(__dirname + "/views/404.html")
       }
-      
+
       const username = userThatWasFound.username
       const exerciseCount = userThatWasFound.exerciseCount
       const log = userThatWasFound.log
-      
-      return res.render("user.ejs", { u: username, e: exerciseCount, l: log})
+
+      return res.render("user.ejs", { u: username, e: exerciseCount, l: log })
       // return res.send(username)
     }
   )
 })
-
-
 
 // create a new user. pass a username to the body, using postman
 app.post("/api/users", function (req, res) {
@@ -107,6 +101,46 @@ app.post("/api/users/id/exercises", function (req, res) {
     userThatWasFound.save()
 
     return res.send("updated the logs array of " + userThatWasFound.username)
+  })
+})
+
+// retrieve a user's (search by id) exercises from date1 to date2
+// http://localhost:3000/api/users/fromto/62e725d783e74b9da58acbff/2000-1-1/2000-1-2
+// the above gets Bobs logged exercises from january 1 to january 2
+app.get("/api/users/fromto/:_id/:from/:to", function (req, res) {
+  let id = req.params._id
+  let from = req.params.from
+  let to = req.params.to
+
+  UserModel.findOne({ _id: id }, function (err, personThatWasFound) {
+    if (err) {
+      return console.error(err)
+    }
+
+    if (!personThatWasFound) {
+      return res.sendFile(__dirname + "/views/404.html")
+    }
+    let parseFrom = Date.parse(from) // ex. going from '2000-1-1' to the number of milliseconds
+    let parseTo = Date.parse(to)
+
+    // we will iterate through the logs array, checking if each log is within the millisecond boundary
+    console.log(personThatWasFound.log)
+    console.log(parseFrom + " " + parseTo)
+
+    const output = personThatWasFound.log.filter(function (
+      currentValue,
+      index
+    ) {
+      const toMilliseconds = Date.parse(currentValue.date)
+      console.log(toMilliseconds)
+      if (toMilliseconds >= parseFrom && toMilliseconds <= parseTo) {
+        return currentValue
+      }
+    })
+    if (output.length == 0) {
+      return res.send("no entries between " + from + " and " + to)
+    }
+    return res.json(output)
   })
 })
 
